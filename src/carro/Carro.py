9de090ecd4889b854d602acbb6ffe5d9314b8fcb
João de Jesus Costa <joao.j.costa@criticalsoftware.com>
2022-07-28 17:10:57 +0100

    Add: parking brake

--- src/carro/Carro.py
+++ src/carro/Carro.py
@@ -6,6 +6,13 @@ import Engine
 class Carro:
     state: CarroState = CarroState.Parked
     weight: int = 1500  # in Kg
+    parkingBrakeState: bool = True  # False - not active || True - active
 
     brakeSystem: BrakeSystem = BrakeSystem()
     engine: Engine = Engine()
+
+    def setParkingBrakeState(self, state):
+        self.parkingBrakeState = state
+
+    def getParkingBrakeState(self):
+        return self.parkingBrakeState
