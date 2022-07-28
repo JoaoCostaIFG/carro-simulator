514298aeb3818723be772349a6f79b50538cf264
João de Jesus Costa <joao.j.costa@criticalsoftware.com>
2022-07-28 16:58:40 +0100

    Ignoring __pycache__

--- src/carro_main.py
+++ src/carro_main.py
@@ -3,6 +3,7 @@
 import can
 
 import carro.Carro
+from carro.carro import Carro
 
 
 def send_one():
@@ -32,5 +33,6 @@ def recv_one():
 
 
 if __name__ == "__main__":
-    send_one()
+    Carro()
+    # send_one()
     # recv_one()
